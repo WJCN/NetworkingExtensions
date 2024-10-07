ff84e9f4488ae9fcc2544be2ced5faab56d1b703
William J. C. Nesbitt <wjcn@mac.com>
2024-10-07 07:06:20 -0700

    Removes ability to customize JSONDecoder.

---
+++ Sources/NetworkingExtensions/JSONDecoderExtension.swift
@@ -0,0 +1,16 @@
+//
+//  JSONDecoderExtension.swift
+//  NetworkingExtensions
+//
+//  Created by William J. C. Nesbitt on 10/7/24.
+//
+
+import Foundation
+
+extension JSONDecoder {
+	@inlinable
+	public func decode<T: Decodable>(_ type: T.Type, from request: URLRequest) async throws -> (T, URLResponse) {
+		let (data, response) = try await URLSession.shared.data(for: request)
+		return try (decode(T.self, from: data), response)
+	}
+}
