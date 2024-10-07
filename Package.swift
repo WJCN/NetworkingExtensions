60284566f233aa6719af494291ad9ac5d2f00e39
William J. C. Nesbitt <wjcn@mac.com>
2024-10-07 02:57:19 -0700

    Adds supprt for all Apple platforms.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,12 @@ import PackageDescription
 let package = Package(
 	name: "NetworkingExtensions",
 	platforms: [
-		.iOS  (.v15),
-		.macOS(.v12),
+		.iOS        (.v15),
+		.macCatalyst(.v15),
+		.macOS      (.v12),
+		.tvOS       (.v15),
+		.visionOS   (.v1),
+		.watchOS    (.v8),
 	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
