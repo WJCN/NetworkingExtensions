4d03c45ae4b2ac281e6bfd46ac4a1d212001cb90
William J. C. Nesbitt <wjcn@mac.com>
2024-10-27 16:53:35 -0700

    Adds Encodable and Decodable for convenience.

--- Sources/NetworkingExtensions/NetworkingExtensions.swift
+++ Sources/NetworkingExtensions/NetworkingExtensions.swift
@@ -27,12 +27,18 @@ extension URLRequest {
 	}
 
 	public init(
-		method:      HTTPMethod,
-		url:         URL,
-		bearerToken: String? = nil,
-		body:        Data?   = nil
+		method:          HTTPMethod,
+		url:             URL,
+		bearerToken:     String?      = nil,
+		body:            Data?        = nil,
+		cachePolicy:     CachePolicy  = .useProtocolCachePolicy,
+		timeoutInterval: TimeInterval = 60
 	) {
-		self.init(url: url)
+		self.init(
+			url:             url,
+			cachePolicy:     cachePolicy,
+			timeoutInterval: timeoutInterval
+		)
 		self.httpMethod = method.rawValue
 		if let bearerToken {
 			self.setValue("Bearer \(bearerToken)", forHTTPHeaderField: "Authorization")
@@ -43,22 +49,71 @@ extension URLRequest {
 			self.httpBody = body
 		}
 	}
+
+	public init(
+		method:          HTTPMethod,
+		url:             URL,
+		bearerToken:     String?      = nil,
+		body:            Encodable?   = nil,
+		encoder:         JSONEncoder  = JSONEncoder(),
+		cachePolicy:     CachePolicy  = .useProtocolCachePolicy,
+		timeoutInterval: TimeInterval = 60
+	) throws {
+		var data: Data?
+		if let body {
+			data = try encoder.encode(body)
+		}
+		self.init(
+			method:          method,
+			url:             url,
+			bearerToken:     bearerToken,
+			body:            data,
+			cachePolicy:     cachePolicy,
+			timeoutInterval: timeoutInterval
+		)
+	}
 }
 
 // MARK: -
 
 extension URLSession {
-	public func httpData(from url: URL) async throws -> (Data, HTTPURLResponse) {
-		let (data, urlResponse) = try await data(from: url)
+	public func httpData(
+		from url: URL,
+		delegate: URLSessionTaskDelegate? = nil
+	) async throws -> (Data, HTTPURLResponse) {
+		let (data, urlResponse) = try await data(from: url, delegate: delegate)
 		guard let httpURLResponse = urlResponse as? HTTPURLResponse
 		else { throw URLError(.badServerResponse) }
 		return (data, httpURLResponse)
 	}
 
-	public func httpData(for request: URLRequest) async throws -> (Data, HTTPURLResponse) {
-		let (data, urlResponse) = try await data(for: request)
+	public func httpData(
+		for request: URLRequest,
+		delegate:    URLSessionTaskDelegate? = nil
+	) async throws -> (Data, HTTPURLResponse) {
+		let (data, urlResponse) = try await data(for: request, delegate: delegate)
 		guard let httpURLResponse = urlResponse as? HTTPURLResponse
 		else { throw URLError(.badServerResponse) }
 		return (data, httpURLResponse)
+	}
+
+	public func httpDecode<T: Decodable>(
+		_    type:    T.Type,
+		from url:     URL,
+		with decoder: JSONDecoder             = JSONDecoder(),
+		delegate:     URLSessionTaskDelegate? = nil
+	) async throws -> (T, HTTPURLResponse) {
+		let (data, response) = try await httpData(from: url, delegate: delegate)
+		return (try decoder.decode(type, from: data), response)
+	}
+
+	public func httpDecode<T: Decodable>(
+		_    type:    T.Type,
+		for  request: URLRequest,
+		with decoder: JSONDecoder             = JSONDecoder(),
+		delegate:     URLSessionTaskDelegate? = nil
+	) async throws -> (T, HTTPURLResponse) {
+		let (data, response) = try await httpData(for: request, delegate: delegate)
+		return (try decoder.decode(type, from: data), response)
 	}
 }
